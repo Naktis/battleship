6cbe5ebde8c71c433448761b7237d82e494a72e1
Augustina <37456430+Naktis@users.noreply.github.com>
2020-12-30 22:07:17 +0200

    Add ships generation

--- js/main.js
+++ js/main.js
@@ -32,12 +32,99 @@ document.addEventListener('DOMContentLoaded', () => {
 
       for (let column = 1; column <= 10; column ++) {
         const square = document.createElement('div');
-        square.dataset.id = [row, column];
+        square.id = [row, column];
         grid.appendChild(square);
       }
     }
   }
 
+  function generate(shipSize, shipName) {
+    let randomDirection = Math.floor(Math.random() * 2);
+
+    const availableStartPos = (function() {
+      let maxrow = 10, maxcol = 10;
+      if (randomDirection === 0) // horizontal
+        maxrow = 11 - shipSize;
+      else maxcol = 11 - shipSize; // vertical
+
+      let positions = [];
+      for (let row = 1; row <= maxrow; row ++) {
+        for (let column = 1; column <= maxcol; column ++) {
+          var shipSquare = 0;
+          while (shipSquare < shipSize) {
+            if (randomDirection === 0 && computerBoard[row+shipSquare][column] !== 0)
+              break;
+            else if (randomDirection === 1 && computerBoard[row][column+shipSquare] !== 0)
+              break;
+            shipSquare ++;
+          }
+
+          if (shipSquare === shipSize)
+            positions.push([row, column]);
+        }
+      }
+      return positions;
+    })();
+
+    let startPosIndex = Math.floor(Math.random() * availableStartPos.length);
+    let startPos = availableStartPos[startPosIndex];
+
+    let shipObject = {name: shipName, row: startPos[0], column: startPos[1], size: shipSize,  direction: randomDirection};
+    placeShip(computerBoard, shipObject);
+  }
+  
+  function placeShip(board, ship) {
+    let row, column;
+    for (let i = 0; i < ship.size; i ++) {
+      if (ship.direction === 0) {
+        row = ship.row + i;
+        column = ship.column;
+      } else {
+        row = ship.row;
+        column = ship.column + i;
+      }
+      board[row][column] = 1;
+      document.getElementById(row + ',' + column).classList.remove("unavailable");
+      document.getElementById(row + ',' + column).classList.add("taken", ship.name);
+
+      if (row !== 1) markUnavailable(board, row-1, column);
+      if (row !== 10) markUnavailable(board, row+1, column);
+      if (column !== 1) markUnavailable(board, row, column-1);
+      if (column !== 10) markUnavailable(board, row, column+1);
+      if (row !== 1 && column !== 1) markUnavailable(board, row-1, column-1);
+      if (row !== 1 && column !== 10) markUnavailable(board, row-1, column+1);
+      if (row !== 10 && column !== 10) markUnavailable(board, row+1, column+1);
+      if (row !== 10 && column !== 1) markUnavailable(board, row+1, column-1);
+    }
+  }
+
+  function markUnavailable(board, row, column) {
+    let square = document.getElementById(row + ',' + column);
+      if (!square.classList.contains("taken")) {
+        square.classList.add("unavailable");
+        board[row][column] = 2;
+      }
+  }
+
   createBoard(userGrid, userBoard);
   createBoard(computerGrid, computerBoard);
+
+  generate(4, "four-size-ship");
+  generate(3, "three-size-ship");
+  generate(3, "three-size-ship");
+  generate(2, "two-size-ship");
+  generate(2, "two-size-ship");
+  generate(2, "two-size-ship");
+  generate(1, "one-size-ship");
+  generate(1, "one-size-ship");
+  generate(1, "one-size-ship");
+  generate(1, "one-size-ship");
+
+  console.log(computerBoard);
 })
+
+// 0 - available
+// 1 - taken
+// 2 - unavailable
+// 3 - hit
+// 4 - missed
